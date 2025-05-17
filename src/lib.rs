4e5c2ba17826e3ea026626f4835367e13a64717a
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-05-17 17:53:35 -0400

    working discord notification

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,19 @@ pub async fn monitor_websites(urls: Vec<String>, timeout_secs: u64, check_interv
     }
 }
 
+pub async fn send_discord_notification(
+    webhook_url: &str,
+    message: &str,
+) -> Result<(), reqwest::Error> {
+    let client = Client::new();
+    let payload = serde_json::json!({
+        "content": message,
+    });
+
+    client.post(webhook_url).json(&payload).send().await?;
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -49,5 +62,16 @@ mod tests {
     async fn test_nonexistent_url_is_down() {
         let result = is_url_up("http://nonexistent.subdomain.rust-lang.org", 5).await;
         assert!(!result, "Expected nonexistent URL to be down");
+    }
+
+    #[tokio::test]
+    async fn test_discord_notification() {
+        let webhook_url = "https://discord.com/api/webhooks/1277781729754484777/AUQiUl_M_suiJ8u7QIDOab1gft7_KFnTXwX93UQZmqPWKhqgNu7a7o0rLe_Zb71EgC-R";
+        let message = "Test notification from Rust!";
+        let result = send_discord_notification(webhook_url, message).await;
+        assert!(
+            result.is_ok(),
+            "Expected notification to be sent successfully"
+        );
     }
 }
