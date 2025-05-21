0076237a7ce6fc464a00b84c90b086c9164cda95
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-05-20 22:22:17 -0400

    init from env

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #[tokio::main]
 async fn main() {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
     downdetector::monitor_websites().await;
 }
